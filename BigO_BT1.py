61369605808ca37a4931ae1426d147c9d47e84e4
quangnd <quang@Killer-b-MBA.local>
2024-11-24 20:17:57 +0700

    update 3

--- BigO_BT1.py
+++ BigO_BT1.py
@@ -5,9 +5,9 @@
 
 #1: Phân tích
     #1: Đối với vòng lặp:
-        # while i < n: n lần
-        # i = i + 1: 2n lần
-        # Input càng lớn thì số lần lặp càng nhiều, với vòng lặp while trên thì tổng số lần lặp của toàn bộ các dòng lệnh là : 1 + 3n
+        # while i < n: N lần
+        # i = i + 1: 2N lần
+        # Input càng lớn thì số lần lặp càng nhiều, với vòng lặp while trên thì tổng số lần lặp của toàn bộ các dòng lệnh là : 1 + 3N
         # Ví dụ: n = 10, số lần lặp là: 1 + 3*10 = 31 lần thực thi
     #2: Đối với đệ quy:
         # Đệ quy bản chất là 1 hàm gọi lại chính nó bên trong hàm đó
@@ -15,8 +15,8 @@
         
     #3: Đối với thao tác khác:
         # i = 0: 1 lần (i nằm ngoài vòng lặp)
-        # while i < n: n lần
-        # i = i + 1: 2n lần (2 phép toán)
+        # while i < n: N lần
+        # i = i + 1: 2N lần (2 phép toán)
 #2: Output
-    # F(n) = 1 + n + 2n (1 + 3n)
-    # O(n)
+    # F(N) = 1 + N + 2N (1 + 3N)
+    # O(N)
